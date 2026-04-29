cc9cfd58cbaf3b6096de1434f9456d854a4e6bd9
polin-sv <polinasvetlay@icloud.com>
2026-04-29 17:24:34 +0300

    test02

--- task01/test01.cpp
+++ task01/test01.cpp
@@ -1,16 +1,16 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-	int* ptr = NULL;
-
-	ptr = new int;
-
-	*ptr = 10;
-
-	cout << "address = " << ptr << " - value = " << *ptr << endl;
-
-	delete ptr;
-
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	int* ptr = NULL;
+//
+//	ptr = new int;
+//
+//	*ptr = 10;
+//
+//	cout << "address = " << ptr << " - value = " << *ptr << endl;
+//
+//	delete ptr;
+//
+//	return 0;
+//}
